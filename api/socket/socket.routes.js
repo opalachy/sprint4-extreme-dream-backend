1607dfdc3e3d3d8cd04165f3d1dfdb132b0f3834
raniel <rbhrbh65@gmail.com>
2020-07-24 23:26:30 +0300

    filter

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -4,21 +4,11 @@ module.exports = connectSockets
 function connectSockets(io) {
     io.on('connection', socket => {
         console.log('show must go on')
-        socket.on('show to everyone booking', msg=> {
-            console.log(msg)
-            io.emit('booking', 'booking')
+        socket.on('booking', sellerId => {
+            console.log(sellerId)
+            io.emit('update exp', 'booking')
+            io.emit(sellerId , 'go to profile someone booking')
+
         })
-
-        // socket.on('update the seller' , SellerId =>{
-        //     io.emit('bookingToSeller', SellerId)
-        // })
-
-        // socket.on('chat topic', topic=>{
-        //     if (socket.myTopic) {
-        //         socket.leave(socket.myTopic)
-        //     }
-        //     socket.join(topic)
-        //     socket.myTopic = topic;
-        // })
     })
 }
