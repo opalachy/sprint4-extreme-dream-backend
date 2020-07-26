a680e5e419cc9ac9e26ad126a5ae537d46690067
Palachy <Oren.Palachy@lamresearch.com>
2020-07-26 17:14:21 +0300

    heroku uploadhh

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 var config;
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   config = require('./prod')
 } else {
